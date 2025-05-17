198bc3cf02b0e648fd9635c680f6e9d5ef0d07a5
versevibes96 <versevibes96@gmail.com>
2025-05-17 22:46:43 +0530

    Update vite.config.js

--- f/vite.config.js
+++ f/vite.config.js
@@ -1,7 +1,8 @@
+// frontend/vite.config.js में ये जोड़ें
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/youtube-downloader/', 
   plugins: [react()],
 })
